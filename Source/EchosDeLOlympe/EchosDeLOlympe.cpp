9e5e4e24a0a02bd81d80985966f48fb990f075f0
UVictoras <vmartinan@gaming.tech>
2025-10-14 15:37:41 +0200

    Initial commit

---
+++ Source/EchosDeLOlympe/EchosDeLOlympe.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EchosDeLOlympe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EchosDeLOlympe, "EchosDeLOlympe" );
