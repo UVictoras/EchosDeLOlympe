3a6b6ab4e1b2fd49ff9ebea74e08c3e78b2919dc
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2025-10-30 17:08:36 +0100

    [DELETE] Reactor Class

--- Source/EchosDeLOlympe/Private/Enemy/Enemy.h
+++ Source/EchosDeLOlympe/Private/Enemy/Enemy.h
@@ -3,15 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Reactors/Reactor.h"
+#include "GameFramework/Pawn.h"
 #include "Enemy.generated.h"
 
-/**
- * 
- */
 UCLASS()
-class AEnemy : public AReactor
+class AEnemy : public APawn
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this pawn's properties
+	AEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 };
